ed8b75596780152ddc614f867ee031a42aea93e4
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-06 15:09:11 +0200

    .

--- cs336_systems/install_nsys.sh
+++ cs336_systems/install_nsys.sh
@@ -1,6 +1,6 @@
 # Add NVIDIA devtools repository (if needed)
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu$(lsb_release -rs)/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/devtools/repos/ubuntu$(lsb_release -rs)/x86_64/ /"
+sudo add-apt-repository -y "deb https://developer.download.nvidia.com/devtools/repos/ubuntu$(lsb_release -rs)/x86_64/ /"
 
-sudo apt -y update
+sudo apt update
 sudo apt -y install nsight-systems
