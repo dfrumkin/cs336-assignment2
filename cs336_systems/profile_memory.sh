c90e9850a521ec42c6c02417870d524a44f99083
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-10 21:18:05 +0200

    profile memory with mixed precision

--- cs336_systems/profile_memory.sh
+++ cs336_systems/profile_memory.sh
@@ -2,12 +2,15 @@
 
 PY=${PYTHON:-"$(which python)"}
 
+mixed_precision=(false true)
 forward_only=(false true)
 ctx=(128 256 512)
 
-for fwd in "${forward_only[@]}"; do
-    for c in "${ctx[@]}"; do
-        "$PY" benchmark.py \
-            forward_only="$fwd" model.context_length="$c" model="2.7B" num_warmup_steps=5 mem_profile=true
+for mx in "${mixed_precision[@]}"; do
+    for fwd in "${forward_only[@]}"; do
+        for c in "${ctx[@]}"; do
+            "$PY" benchmark.py \
+                mixed_precision="$mx" forward_only="$fwd" model.context_length="$c" model="2.7B" num_warmup_steps=5 mem_profile=true
+        done
     done
 done
