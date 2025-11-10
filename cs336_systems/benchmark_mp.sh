c3453ad2dd9531ea62ad00a4cab445680f2fc9d2
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-10 13:47:06 +0200

    Benchmark for mixed precision

---
+++ cs336_systems/benchmark_mp.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo nvidia-smi -pm 0   # turn persistence mode OFF
+
+PY=${PYTHON:-"$(which python)"}
+
+forward_only=(false true)
+models=(small medium large xl "2.7B")
+ctx=(128 256 512 1024)
+
+for fwd in "${forward_only[@]}"; do
+    for m in "${models[@]}"; do
+        for c in "${ctx[@]}"; do
+            sudo nvidia-smi --gpu-reset
+            "$PY" benchmark.py \
+                forward_only="$fwd" model="$m" model.context_length="$c" num_warmup_steps=5 mixed_precision=true
+        done
+    done
+done
+
+# sudo nvidia-smi -pm 1   # turn persistence mode ON - if it was on to begin with
