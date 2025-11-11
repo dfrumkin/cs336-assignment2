dd137bc923faadaef481c1551ccef2252bc478e6
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-11 15:52:37 +0200

    Reset GPU for memory profiling

--- cs336_systems/profile_memory.sh
+++ cs336_systems/profile_memory.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo nvidia-smi -pm 0   # turn persistence mode OFF
+
 PY=${PYTHON:-"$(which python)"}
 
 mixed_precision=(false true)
@@ -9,8 +11,11 @@ ctx=(128 256 512)
 for mx in "${mixed_precision[@]}"; do
     for fwd in "${forward_only[@]}"; do
         for c in "${ctx[@]}"; do
+            sudo nvidia-smi --gpu-reset
             "$PY" benchmark.py \
                 mixed_precision="$mx" forward_only="$fwd" model.context_length="$c" model="2.7B" num_warmup_steps=5 mem_profile=true
         done
     done
 done
+
+# sudo nvidia-smi -pm 1   # turn persistence mode ON - if it was on to begin with
