ee8889ded11ada2f996b5b727bdbe83e7bc06ee2
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-13 08:54:51 +0200

    Compilation

---
+++ cs336_systems/benchmark_comp.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PY=${PYTHON:-"$(which python)"}
+
+compile=(false true)
+forward_only=(false true)
+models=(small medium large xl "2.7B")
+ctx=(128 256 512 1024)
+
+for comp in "${compiled[@]}"; do
+    for fwd in "${forward_only[@]}"; do
+        for m in "${models[@]}"; do
+            for c in "${ctx[@]}"; do
+                "$PY" benchmark.py \
+                    compile="$comp" forward_only="$fwd" model="$m" model.context_length="$c" num_warmup_steps=5 mixed_precision=true use_optimizer=true
+            done
+        done
+    done
+done
