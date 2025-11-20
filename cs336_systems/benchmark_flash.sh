a7ce11bd4418078403ed7d37159ff823274113ce
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-11-20 23:16:41 +0200

    minor

--- cs336_systems/benchmark_flash.sh
+++ cs336_systems/benchmark_flash.sh
@@ -3,5 +3,6 @@ python benchmark_flash.py -m \
     context_length=128,256,512,1024,2048,4096,8192,16384,32768,65536 \
     d_model=16,32,64,128 \
     dtype=bfloat16,float32 \
+    hydra.output_subdir=null \
     hydra/job_logging=disabled \
     hydra/hydra_logging=disabled
